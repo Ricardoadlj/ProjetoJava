16fe937bdaab5bb166395f8ecdeac16cbd8f4db3
Ricardoadlj <rickalvesjunior@outlook.com>
2021-10-18 12:59:30 -0300

    Comitando exemplo

--- primeiro-programaJava/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programaJava/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,7 +1,7 @@
 package cursojava.executavel;
-
+/*Comitando Exemplos*/
 public class PrimeiraClasseJava {
 	public static void main(String[] args) {
-		System.out.println("Hello world :D!!!");
+		System.out.println("Hello world :D!!! Im Barry Allen");
 	}
 }
