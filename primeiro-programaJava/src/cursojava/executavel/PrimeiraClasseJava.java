9f3dd96387b8533fb0d1eb93805805330986a28a
Ricardoadlj <rickalvesjunior@outlook.com>
2021-11-09 18:49:15 -0200

    Printf em java

--- primeiro-programaJava/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programaJava/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,7 +1,16 @@
 package cursojava.executavel;
+
+import java.util.Scanner;
+
 /*Comitando Exemplos*/
 public class PrimeiraClasseJava {
 	public static void main(String[] args) {
-		System.out.println("Hello world :D!!! Im Barry Allen");
+		Scanner input=new Scanner(System.in);
+		double dinheiro,lucro;
+		System.out.println("Informe o valor da sua compra: ");
+		dinheiro = input.nextDouble();
+		lucro = dinheiro * 2.50;
+		System.out.println("O valor da sua compra com juros é: " + lucro);
+		
 	}
 }
